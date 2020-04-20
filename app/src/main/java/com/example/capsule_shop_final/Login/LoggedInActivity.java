31371486c05783c07be07a2d38530460dcb931db
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-04-20 20:08:04 +0530

    Fix crash on app launch due to null UID
    * Move trackChanges() and sendNotification() methods to LoggedInActivity

--- app/src/main/java/com/example/capsule_shop_final/Login/LoggedInActivity.java
+++ app/src/main/java/com/example/capsule_shop_final/Login/LoggedInActivity.java
@@ -2,15 +2,19 @@ package com.example.capsule_shop_final.Login;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
 import com.example.capsule_shop_final.Inventory.AddMedicinesActivity;
+import com.example.capsule_shop_final.MainActivity;
 import com.example.capsule_shop_final.Orders.MyOrders;
+import com.example.capsule_shop_final.Orders.Order;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
@@ -19,14 +23,29 @@ import androidx.core.app.NotificationManagerCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.capsule_shop_final.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.app.Notification.DEFAULT_SOUND;
+import static android.app.Notification.DEFAULT_VIBRATE;
 
 public class LoggedInActivity extends AppCompatActivity {
     final private static String TAG = "LoggedInActivity";
 
     private Button ordersButton;
     private Button inventoryButton;
+
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +56,8 @@ public class LoggedInActivity extends AppCompatActivity {
 
         initialize();
         handle();
+
+        trackChanges();
 
     }
 
@@ -75,6 +96,67 @@ public class LoggedInActivity extends AppCompatActivity {
     private void initialize(){
         ordersButton = findViewById(R.id.btn_my_orders);
         inventoryButton = findViewById(R.id.btn_update_inventory);
+
+        uid = FirebaseAuth.getInstance().getUid();
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("shop");
+    }
+
+
+    private void trackChanges() {
+        DatabaseReference ref = myRef.child(uid).child("orders");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                for (DataSnapshot child : children) {
+                    Order order = child.getValue(Order.class);
+
+                    assert order != null;
+                    String notificationString = String.format("New order from %s", order.getName());
+                    sendNotification("New Order!!", notificationString);
+
+                    Toast.makeText(LoggedInActivity.this, order.getName(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    //    TODO:- ADD HEADS UP POPUP
+    private void sendNotification(String title, String body) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("MyNotifications", "MyNotifications", NotificationManager.IMPORTANCE_HIGH);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        int requestID = (int) System.currentTimeMillis();
+
+        Intent notificationIntent = new Intent(getApplicationContext(), MyOrders.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, requestID,notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "MyNotifications")
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_capsule_blue)
+                .setAutoCancel(true)
+                .setContentText(body)
+                .setDefaults(DEFAULT_SOUND | DEFAULT_VIBRATE)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(contentIntent);
+
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(999, builder.build());
     }
 
 }
