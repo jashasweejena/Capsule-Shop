da558eb78b001d7f916c42b8f719093951308437
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-04-20 12:18:57 +0530

    Fix: Append new medicine to existing list instead of overwriting it

--- app/src/main/java/com/example/capsule_shop_final/Inventory/AddMedicinesActivity.java
+++ app/src/main/java/com/example/capsule_shop_final/Inventory/AddMedicinesActivity.java
@@ -1,24 +1,34 @@
 package com.example.capsule_shop_final.Inventory;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import com.example.capsule_shop_final.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddMedicinesActivity extends AppCompatActivity {
     final private static String TAG = "AddMedicinesActivity";
+
+    private MutableLiveData<List<Medicine>> mutableFetchedList = new MutableLiveData<>();
 
     private EditText descEditText;
     private EditText priceEditText;
@@ -28,7 +38,6 @@ public class AddMedicinesActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     private String uid;
-    private List<Medicine> medsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,29 +48,47 @@ public class AddMedicinesActivity extends AppCompatActivity {
 
         initialize();
 
-        addToListBtn.setOnClickListener(new View.OnClickListener() {
+        final FloatingActionButton fab = findViewById(R.id.fab);
+
+
+        getCurrentList().observe(this, new Observer<List<Medicine>>() {
+
             @Override
-            public void onClick(View view) {
-                String desc = descEditText.getText().toString();
-                int price = Integer.parseInt(priceEditText.getText().toString());
-                int qty = Integer.parseInt(qtyEditText.getText().toString());
+            public void onChanged(final List<Medicine> medicines) {
+                try {
 
-                Medicine medicine = new Medicine(desc, price, qty);
-                medsList.add(medicine);
+                    //Add medicines to list
+                    addToListBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String desc = descEditText.getText().toString();
+                            int price = Integer.parseInt(priceEditText.getText().toString());
+                            int qty = Integer.parseInt(qtyEditText.getText().toString());
 
-                descEditText.setText("");
-                qtyEditText.setText("");
-                priceEditText.setText("");
-            }
-        });
+                            Medicine medicine = new Medicine(desc, price, qty);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                            //Update the fetched list with new data
+                            medicines.add(medicine);
 
-                if(medsList != null && medsList.size() > 0){
-                    writeData(medsList);
+                            descEditText.setText("");
+                            qtyEditText.setText("");
+                            priceEditText.setText("");
+                        }
+                    });
+
+                    //Update list and push to Firebase
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            if (medicines != null && medicines.size() > 0) {
+                                writeData(medicines);
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    Log.d(TAG, "onChanged: " + e);
+                    Toast.makeText(AddMedicinesActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -80,10 +107,30 @@ public class AddMedicinesActivity extends AppCompatActivity {
 
         uid = FirebaseAuth.getInstance().getUid();
 
-        medsList = new ArrayList<>();
-
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference(String.format("shop/%s", uid));
+    }
+
+    private MutableLiveData<List<Medicine>> getCurrentList() {
+        final List<Medicine> medList = new ArrayList<>();
+
+        myRef.child("medicines").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Medicine medicine = child.getValue(Medicine.class);
+                    medList.add(medicine);
+                }
+                mutableFetchedList.postValue(medList);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return mutableFetchedList;
     }
 
 }
