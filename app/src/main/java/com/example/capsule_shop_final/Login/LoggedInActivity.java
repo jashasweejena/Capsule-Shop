add8c60f9d8b6b0f373bc32b090e1a4dc81bea98
Jashaswee Jena <j.ishujena2000@gmail.com>
2020-04-20 18:42:11 +0530

    NotificationManager works

--- app/src/main/java/com/example/capsule_shop_final/Login/LoggedInActivity.java
+++ app/src/main/java/com/example/capsule_shop_final/Login/LoggedInActivity.java
@@ -1,6 +1,9 @@
 package com.example.capsule_shop_final.Login;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.capsule_shop_final.Inventory.AddMedicinesActivity;
@@ -10,6 +13,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.util.Log;
 import android.view.View;
@@ -32,6 +37,22 @@ public class LoggedInActivity extends AppCompatActivity {
 
         initialize();
         handle();
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("MyNotifications", "MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "MyNotifications")
+                .setContentTitle("This is my title")
+                .setSmallIcon(R.drawable.ic_capsule_blue)
+                .setAutoCancel(true)
+                .setContentText("This is my text");
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(999, builder.build());
     }
 
     private void handle() {
@@ -40,6 +61,20 @@ public class LoggedInActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoggedInActivity.this, MyOrders.class);
                 startActivity(intent);
+
+//              For testing only
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(LoggedInActivity.this, "channel_id")
+                        .setSmallIcon(android.R.drawable.star_on)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        // Set the intent that will fire when the user taps the notification
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(LoggedInActivity.this);
+
+// notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, builder.build());
 
             }
         });
